8d16517801c4794b65eefd00b780101247614a34
Mathieu Blanchard <matblanchard.pro@gmail.com>
2025-09-05 18:19:57 +0200

    correction bug 2eme navbar

--- jobard_leaderboard/leaderboard/urls.py
+++ jobard_leaderboard/leaderboard/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("admin-dashboard/", views.admin_dashboard, name="admin_dashboard"),
     path("categories/", views.manage_categories, name="manage_categories"),
-    path("categories/", views.manage_categories, name="manage_users"),
+    path("users/", views.manage_users, name="manage_users"),
 ]
